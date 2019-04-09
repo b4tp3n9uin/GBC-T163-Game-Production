e869e113db993aba16c83861b4549dbbd190efd3
b4tp3n9uin <47507439+b4tp3n9uin@users.noreply.github.com>
2019-04-09 15:41:50 -0400

    Update Bullet.h

--- 1014-project/Bullet.h
+++ 1014-project/Bullet.h
@@ -12,11 +12,19 @@ public:
 	~Bullet(){};
 
 	// Constructor
-	Bullet(float bx, float by){
+	Bullet(float bx, float by, int bType){
 	  bDestR.x = bx;
 	  bDestR.y = by;
 		bSurf = IMG_Load("img/PlayerBullet.png");
 		bText = SDL_CreateTextureFromSurface(Game::Instance()->GetRenderer(), bSurf);
+	}
+
+	Bullet(float bx, float by, int bType, float bAngle) {
+		bDestR.x = bx;
+		bDestR.y = by;
+		bSurf = IMG_Load("img/PlayerBullet.png");
+		bText = SDL_CreateTextureFromSurface(Game::Instance()->GetRenderer(), bSurf);
+		eAngle = bAngle;
 	}
 
 	// Bullet rect
@@ -27,7 +35,12 @@ public:
 	SDL_Texture* bText;
 
 	// Bullet speed
-	int bSpeed = 10;
+	int bSpeed = 300;
+	
+	// Bullet type
+	int bulletType;
+	// Bullet angle
+	float eAngle;
 
 	// Bullet location
 	Vector2 bLocation;
@@ -36,9 +49,50 @@ public:
 	void Update();
 	void Render();
 	void Clean();
-
+	
 	// GetBullet location
 	Vector2& GetBulletLocation(){ return bLocation; }
 
+
+};
+
+class Bomb {
+public:
+	Bomb();
+	~Bomb() {};
+
+	// Constructor
+	Bomb(float bx, float by) {
+		bDestR.x = bx;
+		bDestR.y = by;
+		bSurf = IMG_Load("img/BeforeBomb.png");
+		bText = SDL_CreateTextureFromSurface(Game::Instance()->GetRenderer(), bSurf);
+	}
+
+	// Bomb rect
+	SDL_Rect bDestR;
+
+	// Bomb texture
+	SDL_Surface* bSurf;
+	SDL_Texture* bText;
+
+	//Bomb Timer
+	float bombTimer;
+	bool explode = false;
+	bool trigger = false;
+	bool imageChange = false;
+	// Bomb speed
+	int bSpeed = 300;
+
+	// Bomb location
+	Vector2 bLocation;
+
+	// Update & Render
+	void Update();
+	void Render();
+	void Clean();
+
+	// GetBomb location
+	Vector2& GetBombLocation() { return bLocation; }
 
 };
