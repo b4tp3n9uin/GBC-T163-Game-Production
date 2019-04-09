03a828108e75b3ed882e69db52ed9d1a8bcee32e
b4tp3n9uin <47507439+b4tp3n9uin@users.noreply.github.com>
2019-04-09 15:39:13 -0400

    Update Player.cpp

--- 1014-project/Player.cpp
+++ 1014-project/Player.cpp
@@ -6,10 +6,22 @@ using namespace std;
 
 void Player::SpawnBullet(){
   // Push a new bullet obj into the bullet vector
-	m_vBullets.push_back(new Bullet(pLocation.x + 30, pLocation.y + 20));
+	m_vBullets.push_back(new Bullet(pLocation.x + 20, pLocation.y + 15, bulletType));
 }
 
+void Player::SpawnBullet(float angle) {
+	// Push a new bullet obj into the bullet vector
+	m_vBullets.push_back(new Bullet(pLocation.x + 20, pLocation.y + 15, bulletType, angle));
+}
 
+void Player::SpawnBomb() {
+	// Push a new bomb obj into the bullet vector
+	if (pBombNum > 0)
+	{
+		m_Bomb = new Bomb(pLocation.x + 30, pLocation.y + 20);
+		pBombNum--;
+	}
+}
 
 
 
@@ -19,8 +31,8 @@ void Player::Update(){
 
 
   // Update the player size
-  pDestR.h = 64;
-	pDestR.w = 64;
+  pDestR.h = 45;
+	pDestR.w = 45;
 
   // Update Player location according to its movement
   pLocation.x = pDestR.x;
@@ -42,6 +54,11 @@ void Player::Update(){
     }
   }
   m_vBullets.shrink_to_fit();
+  
+  // Update Bomb
+  if(m_Bomb)
+	m_Bomb->Update();
+  
 }
 
 
@@ -58,6 +75,10 @@ void Player::Render(){
 	for (int i = 0; i < m_vBullets.size(); i++){
 		m_vBullets[i] -> Render();
 	}
+
+	// Render Bomb
+	if (m_Bomb)
+		m_Bomb->Render();
 }
 
 
@@ -71,6 +92,9 @@ void Player::Clean(){
 	}
 	m_vBullets.clear();
 	m_vBullets.shrink_to_fit();
+
+	if (m_Bomb)
+		m_Bomb->Clean();
 
 	// Delete player graphics
 	SDL_FreeSurface(pSurf);
