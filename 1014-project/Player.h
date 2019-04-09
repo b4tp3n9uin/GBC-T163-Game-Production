5cbdc4ecd2421c7cdfd012191e73767178b4dd71
b4tp3n9uin <47507439+b4tp3n9uin@users.noreply.github.com>
2019-04-09 15:39:10 -0400

    Update Player.h

--- 1014-project/Player.h
+++ 1014-project/Player.h
@@ -6,15 +6,16 @@
 using namespace std;
 
 class Bullet;
+class Bomb;
 class Player{
 public:
 	Player(){};
-	~Player() ;
 
 	// Constructor set player location
 	Player(float px, float py){
 		pDestR.x = px;
 		pDestR.y = py;
+		isDead = false;
 	}
 
 	// Player rect
@@ -27,6 +28,13 @@ public:
 	// Player speed
 	int pSpeed;
 
+	// player Bomb number
+	int pBombNum = 3;
+	// player Bullet type
+	int bulletType = 0;
+
+	// is Dead
+	bool isDead;
 
 	// Player location
 	Vector2 pLocation;
@@ -35,14 +43,17 @@ public:
 	vector<Bullet*> m_vBullets;
 	int bulletCount;
 
-
+	// Bomb
+	Bomb* m_Bomb;
 
 	// Functions
 	void SpawnBullet();
+	void SpawnBullet(float angle);
+	void SpawnBomb();
 	void Render();
 	void Update();
 	void Clean();
-
+	
 	// Get all bullets
 	vector<Bullet*>& GetBullets() { return m_vBullets; }
 
